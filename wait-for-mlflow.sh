3e1cbd1a66e519b55020bbf8606050e490a98185
nithi-code <speaktonithi@gmail.com>
2025-10-10 04:11:40 +0530

    Changes

--- wait-for-mlflow.sh
+++ wait-for-mlflow.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-MLFLOW_URL=http://mlflow-server:5000
-echo "Waiting for MLflow server at $MLFLOW_URL..."
-while ! curl -s $MLFLOW_URL >/dev/null; do
-    sleep 2
+MLFLOW_URL=${MLFLOW_TRACKING_URI:-http://mlflow-server:5000}
+
+echo "Waiting for MLflow server at ${MLFLOW_URL}..."
+while ! curl -s "${MLFLOW_URL}/api/2.0/mlflow/experiments/list" > /dev/null 2>&1; do
+    echo "MLflow not ready yet. Sleeping 5s..."
+    sleep 5
 done
-echo "MLflow server is up. Starting FastAPI..."
-exec "$@"
+echo "MLflow server is ready!"
